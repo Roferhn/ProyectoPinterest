b238d8642be402289e323f76096611c32c5b5ef4
Roferhn <58787561+Roferhn@users.noreply.github.com>
2020-08-11 13:53:10 -0600

    implementacion de la autentificacion

--- app/src/main/java/com/example/proyectopinterest/MainActivity.java
+++ app/src/main/java/com/example/proyectopinterest/MainActivity.java
@@ -1,14 +1,94 @@
 package com.example.proyectopinterest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Antut";
+    private EditText EtEmail,EtPassword;
+    private Button  BtnLogin;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        EtEmail = (EditText) findViewById(R.id.email);
+        EtPassword = (EditText) findViewById(R.id.password);
+        BtnLogin = (Button) findViewById(R.id.login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+
+                if (firebaseAuth.getCurrentUser()!=null){
+                    startActivity(new Intent(MainActivity.this,MainActivity2.class));
+                }else {
+
+                    Toast.makeText(MainActivity.this, "Datos incoreectos", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
+
+        BtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+        
+
+
+
+
+    }
+    
+    private void LogUser(){
+
+        String email = EtEmail.getText().toString();
+        String password = EtPassword.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+        
     }
 }
