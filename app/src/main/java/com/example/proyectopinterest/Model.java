ac97c70d2f665b908798df0576b67d6a4a47ee22
Roferhn <58787561+Roferhn@users.noreply.github.com>
2020-08-15 17:11:36 -0600

    clase Model y constructores

---
+++ app/src/main/java/com/example/proyectopinterest/Model.java
@@ -0,0 +1,14 @@
+package com.example.proyectopinterest;
+
+public class Model {
+
+    private int image;
+
+    public Model(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
