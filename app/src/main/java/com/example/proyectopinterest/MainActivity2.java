5042d4c42de1d539457b15e0c9cfc957e7039882
Roferhn <58787561+Roferhn@users.noreply.github.com>
2020-08-15 17:10:26 -0600

    Recilerview activity_main2.xml

--- app/src/main/java/com/example/proyectopinterest/MainActivity2.java
+++ app/src/main/java/com/example/proyectopinterest/MainActivity2.java
@@ -4,53 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-    }
-
-    private void GetDatos(){
-
-        String url = "https://api.rawg.io/api/games";
-
-
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-
-                try {
-                    JSONArray Juegos = response.getJSONArray("results");
-
-                    for(int i=0; i<Juegos.length(); i++){
-                        JSONObject object = Juegos.getJSONObject(i);
-                        String name = object.getString("name");
-
-                        TVResult.append(name + "\n\n");
-                    }
-
-                }catch (JSONException e){
-                    e.printStackTrace();
-                }
-
-
-
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
-
-
-        });
-        queue.add(request);
     }
 }
