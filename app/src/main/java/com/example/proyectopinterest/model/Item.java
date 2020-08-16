cc01cd7af6e23c5dbde4be06698cc1fef0db1021
Roferhn <58787561+Roferhn@users.noreply.github.com>
2020-08-16 16:08:01 -0600

    remplazo model.java por intem.java

--- app/src/main/java/com/example/proyectopinterest/model/Item.java
+++ app/src/main/java/com/example/proyectopinterest/model/Item.java
@@ -1,5 +1,28 @@
-package com.example.proyectopinterest.model;
+public class Item {
 
-import java.lang.ref.SoftReference;
+    private String mImageURL;
+    private String mLikes;
+    private String mAutor;
+
+    public Item() {
+    }
+
+    public Item(String ImageURL, String Likes, String Autor) {
+        mImageURL = ImageURL;
+        mLikes = Likes;
+        mAutor = Autor;
+    }
 
 
+    public String getAutor() {
+        return mAutor;
+    }
+
+    public String getImageURL() {
+        return mImageURL;
+    }
+
+    public String getLikes() {
+        return mLikes;
+    }
+}
