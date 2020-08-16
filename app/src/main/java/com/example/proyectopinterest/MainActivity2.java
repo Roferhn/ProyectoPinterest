e9056eae5a683b78e6e95f608b27c748c3c0863b
Roferhn <58787561+Roferhn@users.noreply.github.com>
2020-08-16 17:18:15 -0600

    Creacion Consulta a la api en MainActivity2.java

--- app/src/main/java/com/example/proyectopinterest/MainActivity2.java
+++ app/src/main/java/com/example/proyectopinterest/MainActivity2.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.view.Display;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.proyectopinterest.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -36,4 +44,45 @@ public class MainActivity2 extends AppCompatActivity {
         parseJson();
 
     }
+
+    private void parseJson() {
+
+        String url= "https://pixabay.com/api/?key=17928583-32797671385677407f1267da7&q=kitten&image_type=photo&pretty=true";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("hits");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject hit = jsonArray.getJSONObject(i);
+
+                                String autorName = hit.getString("user");
+                                String imageUrl = hit.getString("webformatURL");
+                                int likeCount = hit.getInt("likes");
+
+                                mList.add(new ModelItem(autorName, imageUrl, likeCount));
+
+                            }
+
+                            mAdapter = new Adapter(MainActivity2.this, mList);
+                            mRecyclerView.setAdapter(mAdapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mRequestQueue.add(request);
+
+    }
+
+
 }
