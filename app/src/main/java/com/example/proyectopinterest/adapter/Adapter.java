d6f33c0eef2631cc65cd68e4b07af4aa480e48d9
Roferhn <58787561+Roferhn@users.noreply.github.com>
2020-08-16 16:10:21 -0600

    reinicio de la clase Adapter.java

--- app/src/main/java/com/example/proyectopinterest/adapter/Adapter.java
+++ app/src/main/java/com/example/proyectopinterest/adapter/Adapter.java
@@ -1,61 +1,6 @@
 package com.example.proyectopinterest.adapter;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.proyectopinterest.model.Item;
-import com.example.proyectopinterest.R;
-import com.makeramen.roundedimageview.RoundedImageView;
-
-import java.util.List;
-
-public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
-
-    private List<Item> items;
-
-    public Adapter(List<Item> items) {
-        this.items = items;
-    }
-
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ViewHolder(
-                LayoutInflater.from(parent.getContext()).inflate(
-                        R.layout.item,
-                        parent,
-                        false
-                )
-        );
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.setPostImage(items.get(position));
-    }
-
-    @Override
-    public int getItemCount() {
-        return items.size();
-    }
-
-    class ViewHolder extends RecyclerView.ViewHolder{
-
-        RoundedImageView postImageView;
-
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            postImageView = itemView.findViewById(R.id.imageContainer);
-        }
-
-        void setPostImage(Item item){
-            postImageView.setImageResource(Integer.parseInt(item.getImage()));
-        }
-
-    }
-
+public class Adapter extends RecyclerView.Adapter {
 }
